dd1582d2a3ba02ba11353e6f4ca95a01ac9cc54f
Matthias Klass <matthias.klass@gmail.com>
2019-06-08 14:12:54 +0200

    try #2

---
+++ docs/precache-manifest.382539e68d29fd4a1f6f02adcb79cba4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c0890a2f4b1f20713333bdaed32f187",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d856362d4bc63e697f79",
+    "url": "/static/css/2.1c29fa96.chunk.css"
+  },
+  {
+    "revision": "a169b1bea98309a889eb",
+    "url": "/static/css/main.a9816d43.chunk.css"
+  },
+  {
+    "revision": "d856362d4bc63e697f79",
+    "url": "/static/js/2.47eae722.chunk.js"
+  },
+  {
+    "revision": "a169b1bea98309a889eb",
+    "url": "/static/js/main.a15ac202.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
